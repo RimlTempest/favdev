8250e1c02912af9815181402f51bc1e063a4ea38
RimlTempest <riml.slime@gmail.com>
2023-01-09 00:05:03 +0900

    add: daisyui、headlessui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}',"./src/components/**/*.{js,ts,jsx,tsx}",'./src/stories/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/stories/**/*.{js,ts,jsx,tsx}',
+    './src/features/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
